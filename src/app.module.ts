5d6b523bcdb0d5685d75653e57649085bc5c7e3d
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2025-11-07 17:07:35 +0100

    Remove not used import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UtilModule } from './util/config.module';
 import { MidiModule } from './adapter/in/midi/midi.module';
-import { HueModule } from './adapter/out/hue/hue.module';
 import { DeviceCollectionInitializer } from './domain/service/device.collection.service';
 import { LightOnOffService } from './domain/service/light.onoff.service';
 import { LightOnOffUseCase } from './port/in/light.usecase';
