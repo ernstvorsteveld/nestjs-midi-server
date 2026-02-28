d4c2a69f665426d6987f97b89c8415fb12cf8711
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2026-02-28 10:08:39 +0100

    Rollback changes related to "last light used"

--- src/adapter/out/hue/hue.facade.hue.ts
+++ src/adapter/out/hue/hue.facade.hue.ts
@@ -1,13 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import axios, { AxiosRequestConfig } from 'axios';
 import { Device } from 'src/domain/model/device.model';
-import {
-  HueDimmingStatus,
-  HueLight,
-  HueLightUpdatePayload,
-  Light,
-  LightState,
-} from './hue.model';
+import { HueApiResponse, HueLightUpdatePayload, LightState } from './hue.model';
 import { ConfigurationService } from 'src/util/config.service';
 import { LightsPort } from 'src/port/out/lights/lights.port';
 
@@ -30,27 +24,8 @@ export class LightsFacadeHue implements LightsPort {
     };
   }
 
-  async brightness(device: Device): Promise<Light> {
-    const currentBrightness: HueDimmingStatus = await this.getDimmingStatus(
-      device.deviceId,
-    );
-    const uri = `${this.configurationService.hueHost()}/clip/v2/resource/light/${device.deviceId}`;
-    const body: HueLightUpdatePayload = {
-      dimming: {
-        brightness: currentBrightness.brightness,
-      },
-    };
-    try {
-      await axios.put(uri, body, this.config);
-    } catch (error) {
-      console.error(error);
-    }
-
-    return Promise.resolve(newState);
-  }
-
   async flipState(device: Device): Promise<LightState> {
-    const currentState = await this.getLightStatus(device.deviceId);
+    const currentState = await this.getState(device.deviceId);
     const newState =
       currentState === LightState.ON ? LightState.OFF : LightState.ON;
 
@@ -70,45 +45,23 @@ export class LightsFacadeHue implements LightsPort {
     return Promise.resolve(newState);
   }
 
-  private isEmpty(o: any) {
-    return Object.keys(o).length === 0;
-  }
-
-  private async getLightStatus(id: string): Promise<LightState> {
-    const light: HueLight = await this.getLampState(id);
+  async getState(id: string): Promise<LightState> {
+    const uri = `${this.configurationService.hueHost()}/clip/v2/resource/light/${id}`;
+    console.error(uri);
+    const response = await axios.get(uri, this.config);
+    if (response === undefined || response.status !== 200) {
+      return LightState.OFF;
+    }
     try {
-      if (this.isEmpty(light)) {
+      if (response.data === undefined) {
         return LightState.OFF;
       }
-      const state: boolean = light.on.on;
+      const light: HueApiResponse = response.data as HueApiResponse;
+      const state: boolean = light.data[0].on.on;
       return state ? LightState.ON : LightState.OFF;
     } catch (error) {
       console.error(error);
       return LightState.OFF;
     }
-  }
-
-  private async getDimmingStatus(id: string): Promise<HueDimmingStatus> {
-    const light = await this.getLampState(id);
-    try {
-      if (this.isEmpty(light)) {
-        return { brightness: 0 } as HueDimmingStatus;
-      }
-      return { brightness: light.dimming?.brightness } as HueDimmingStatus;
-    } catch (error) {
-      console.error(error);
-      return { brightness: 0 } as HueDimmingStatus;
-    }
-  }
-
-  private async getLampState(id: string): Promise<HueLight> {
-    const uri = `${this.configurationService.hueHost()}/clip/v2/resource/light/${id}`;
-    console.error(uri);
-    const response = await axios.get(uri, this.config);
-    if (response === undefined || response.status !== 200) {
-      return Promise.resolve({} as HueLight);
-    }
-    const lights: HueLight[] = response.data as HueLight[];
-    return lights[0];
   }
 }
--- src/port/out/lights/lights.port.ts
+++ src/port/out/lights/lights.port.ts
@@ -1,9 +1,7 @@
-import { Light, LightState } from 'src/adapter/out/hue/hue.model';
+import { LightState } from 'src/adapter/out/hue/hue.model';
 import { Device } from 'src/domain/model/device.model';
 
 export const LightsPort = Symbol('LightsPort');
 export interface LightsPort {
   flipState(device: Device): Promise<LightState>;
-
-  brightness(device: Device): Promise<Light>;
 }
